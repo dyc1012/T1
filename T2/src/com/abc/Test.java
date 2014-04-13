62887e81e713905b549861b2d4d31c7fb86d08df
dyc <dyc1012@163.com>
2014-04-13 12:43:55 +0800

    t

--- T2/src/com/abc/Test.java
+++ T2/src/com/abc/Test.java
@@ -8,14 +8,8 @@ public class Test
 	 */
 	public static void main(String[] args)
 	{
-		System.out.println("Great~xxxxx~11");
-
 		// xxxxxxxxxxxxxxxxxxxxxxxxxx
 
-		int j = 0;
-
-		System.out.println("Great~xxxxx~11");
-		System.out.println("Great~xxxxx~11");
 	}
 
 }
